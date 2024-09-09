460f816d4179b184ae4651ae8a3bea6582c1a6ce
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2024-09-09 11:06:17 +0200

    feat integrate initRegistry, on models

--- api-interface/models/models.go
+++ api-interface/models/models.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"reflect"
 
 	entity "api-interface/entities"
 	repository "api-interface/repositories"
@@ -9,43 +10,43 @@ import (
 	"go.etcd.io/bbolt"
 )
 
-var (
-    repositories = make(map[string]interface{})
-)
+func init() {
+    entity.RegisterEntityType("Bucket", reflect.TypeOf(entity.Bucket{}))
+    entity.RegisterEntityType("BucketObject", reflect.TypeOf(entity.BucketObject{}))
+    entity.RegisterEntityType("Owner", reflect.TypeOf(entity.Owner{}))
+    registerQueryBuilder[*entity.Bucket]("Bucket")
+    registerQueryBuilder[*entity.BucketObject]("BucketObject")
+    registerQueryBuilder[*entity.Owner]("Owner")
+}
+
+var repositories = make(map[string]interface{})
+var queryBuilderConstructors = make(map[string]func(entity.Entity, *bbolt.DB, string) (interface{}, error))
+
+func registerQueryBuilder[T entity.Entity](typeName string) {
+    queryBuilderConstructors[typeName] = func(e entity.Entity, db *bbolt.DB, bucketName string) (interface{}, error) {
+        return repository.NewQueryBuilder[T](e.(T), db, bucketName)
+    }
+}
 
 func InitRepositories(db *bbolt.DB) {
     fmt.Println(Yellow + "=== Initialisation des Repositories ===" + Reset)
 
-    // Obtenir tous les types enregistrés dans le registre global
     for typeName, structType := range entity.GetAllEntityTypes() {
-
         if AssureEntityCompliance(typeName, structType) {
             bucketName := structType.Name() + "s"
-            // fmt.Printf("Nom du bucket : %s\n", bucketName)
             entityInstance, err := entity.NewEntityInstance(typeName)
             if err != nil {
                 fmt.Printf("%sErreur lors de la création de l'instance de l'entité %s : %s%s\n", Red, typeName, err, Reset)
                 continue
             }
 
-            // Utiliser un switch pour gérer les types d'entités spécifiques
-            var queryBuilder interface{}
-            switch e := entityInstance.(type) {
-            case *entity.Bucket:
-                // on retourn e est on l'exprime en tant que *entity.Bucket
-                fmt.Println("Traitement de l'entité reconnue comme Bucket :", e)
-                queryBuilder, err = repository.NewQueryBuilder[*entity.Bucket](entityInstance.(*entity.Bucket), db, bucketName)
-
-            case *entity.BucketObject:
-                fmt.Println("Traitement de l'entité comme BucketObject :", e)
-                queryBuilder, err = repository.NewQueryBuilder[*entity.BucketObject](entityInstance.(*entity.BucketObject), db, bucketName)
-            case *entity.Owner:
-                fmt.Println("Traitement de l'entité comme Owner:", e)
-                queryBuilder, err = repository.NewQueryBuilder[*entity.Owner](entityInstance.(*entity.Owner), db, bucketName)
-            default:
+            constructor, exists := queryBuilderConstructors[typeName]
+            if !exists {
                 fmt.Printf("%sType d'entité non supporté : %s%s\n", Red, typeName, Reset)
                 continue
             }
+
+            queryBuilder, err := constructor(entityInstance, db, bucketName)
             if err != nil {
                 fmt.Printf("%sErreur lors de la création du QueryBuilder pour l'entité %s : %s%s\n", Red, typeName, err, Reset)
                 continue
