a55eda4067160a37f7c840817639b252145a6eec
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2024-09-14 16:23:17 +0200

    FIX : change body required in optionnal and add params request of bucketName

--- api-interface/middlewares/bucket_creation/bucket_creation_middleware.go
+++ api-interface/middlewares/bucket_creation/bucket_creation_middleware.go
@@ -1,14 +1,15 @@
 package middlewares
 
 import (
-    "encoding/xml"
-    entities "api-interface/entities"
-    "api-interface/handlers/errors"
-    bucket_name_validation "api-interface/middlewares/bucket_name_validation"
-    "github.com/gofiber/fiber/v2"
-    "os"
-    "path/filepath"
-    "strings"
+	entities "api-interface/entities"
+	"api-interface/handlers/errors"
+	bucket_name_validation "api-interface/middlewares/bucket_name_validation"
+	"encoding/xml"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 // BucketValidationMiddleware est le middleware de validation pour les buckets
@@ -16,22 +17,15 @@ func BucketValidationMiddleware() fiber.Handler {
     return func(c *fiber.Ctx) error {
         // Lire le corps de la requête
         bodyRequest := c.Body()
+        bucketName := c.Params("bucketName")
+
 
         // Nouvelle instance de CreateBucketRequestStruct
         bucketRequest := new(entities.CreateBucketRequestStruct)
-
+        if bodyRequest == nil {
         // Décoder le corps XML dans la struct bucketRequest
         if err := xml.Unmarshal(bodyRequest, bucketRequest); err != nil {
             return errors.HandleError(c, errors.ErrUnprocessableEntity, "Erreur de parsing XML: " + err.Error())
-        }
-
-        // Créez un validateur pour le nom du bucket
-        validator := bucket_name_validation.NewBucketNameValidator()
-        result := validator.Validate(bucketRequest.Name)
-
-        if len(result) > 0 {
-            errorMessage := strings.Join(result, "; ")
-            return errors.HandleError(c, errors.ErrBadRequest, errorMessage)
         }
 
         // Valider les champs du bucket
@@ -39,13 +33,26 @@ func BucketValidationMiddleware() fiber.Handler {
             return err
         }
 
+        c.Locals("bucketRequest", bucketRequest)
+    }
+
+
+        // Créez un validateur pour le nom du bucket
+        validator := bucket_name_validation.NewBucketNameValidator()
+        result := validator.Validate(bucketName)
+
+        if len(result) > 0 {
+            errorMessage := strings.Join(result, "; ")
+            return errors.HandleError(c, errors.ErrBadRequest, errorMessage)
+        }
+
         // Vérifier si le bucket existe déjà
-        if bucketExists(bucketRequest.Name) {
+        if bucketExists(bucketName) {
             return errors.HandleError(c, errors.ErrBadRequest, "Le bucket avec ce nom existe déjà.")
         }
 
         // Attacher les données validées au contexte
-        c.Locals("bucketRequest", bucketRequest)
+        c.Locals("bucketName", bucketName)
 
         return c.Next()
     }
